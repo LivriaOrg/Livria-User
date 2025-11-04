fe5c541c140c2f6cc8c9870138cbde8e8a98ef5d
kcc12321 <cassiusmartel21042006@gmail.com>
2025-11-03 19:54:07 -0500

    Add Livria Icons

---
+++ lib/common/utils/app_icons.dart
@@ -0,0 +1,39 @@
+// lib/common/utils/app_icons.dart
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter/widgets.dart';
+
+
+class AppIcons {
+  // --- Barra de Navegación Inferior ---
+  static const IconData home = FontAwesomeIcons.house;
+  static const IconData categories = FontAwesomeIcons.bars;
+  static const IconData community = FontAwesomeIcons.users;
+  static const IconData notifications = FontAwesomeIcons.bell;
+  static const IconData profile = FontAwesomeIcons.user;
+
+  // --- Barra de Aplicación (AppBar) ---
+  static const IconData search = FontAwesomeIcons.magnifyingGlass;
+  static const IconData cart = FontAwesomeIcons.cartShopping;
+  static const IconData location = FontAwesomeIcons.mapPin;
+  static const IconData recommendations = FontAwesomeIcons.lightbulb;
+
+
+  // --- Acciones de Producto ---
+  static const IconData filter = FontAwesomeIcons.filter;
+  static const IconData addFavorite = FontAwesomeIcons.bookmark;
+  static const IconData removeFavorite = FontAwesomeIcons.circleMinus;
+  static const IconData addToCart = FontAwesomeIcons.cartPlus;
+  static const IconData post = FontAwesomeIcons.circlePlus;
+  static const IconData star = FontAwesomeIcons.star;
+  static const IconData trash = FontAwesomeIcons.trash;
+  static const IconData delivery = FontAwesomeIcons.truck;
+  static const IconData creditCard = FontAwesomeIcons.creditCard;
+
+
+  // --- Íconos de Registro/Perfil ---
+  static const IconData camera = FontAwesomeIcons.cameraRetro;
+  static const IconData gallery = FontAwesomeIcons.image;
+  static const IconData logout = FontAwesomeIcons.rightFromBracket;
+  static const IconData deleteAccount = FontAwesomeIcons.trash;
+
+}
