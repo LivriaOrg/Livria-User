a1dbd7b35a4490399844ae066165642d40c2258f
kcc12321 <cassiusmartel21042006@gmail.com>
2025-11-03 22:16:47 -0500

    Create Initial Pages

---
+++ lib/features/auth/presentation/pages/recommendations_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class RecommendationsPage extends StatelessWidget {
+  const RecommendationsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Pantalla Recommendations'),
+    );
+  }
+}
