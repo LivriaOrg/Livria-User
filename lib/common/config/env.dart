0215338e9e4a58f5d8545137c2f81de1a21dd53b
MarceHkd <mrclbinda@gmail.com>
2025-11-13 23:50:56 -0500

    Backend v2

--- lib/common/config/env.dart
+++ lib/common/config/env.dart
@@ -1,6 +1,6 @@
 class Env {
   static const apiBase = String.fromEnvironment(
     'API_BASE',
-    defaultValue: 'https://livriagod.azurewebsites.net',
+    defaultValue: 'https://livriaapp.azurewebsites.net',
   );
 }
