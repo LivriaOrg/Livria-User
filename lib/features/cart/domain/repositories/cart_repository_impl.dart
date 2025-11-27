63ff5c020251d9e92ecc2673876d4d50862d4f5e
kcc12321 <cassiusmartel21042006@gmail.com>
2025-11-27 12:59:00 -0500

    Add Cart Repository

---
+++ lib/features/cart/domain/repositories/cart_repository_impl.dart
@@ -0,0 +1,30 @@
+import '../../domain/entities/cart_item.dart';
+import '../../domain/repositories/cart_repository.dart';
+import '../../infrastructure/datasource/cart_remote_datasource.dart';
+
+class CartRepositoryImpl implements CartRepository {
+  // Inyección del DataSource
+  final CartRemoteDataSource remoteDataSource;
+
+  CartRepositoryImpl({required this.remoteDataSource});
+
+  @override
+  Future<List<CartItem>> getCartItems(int userId) async {
+    return await remoteDataSource.getCartItems(userId);
+  }
+
+  @override
+  Future<void> addItem(int bookId, int quantity, int userId) async {
+    await remoteDataSource.addToCart(bookId, quantity, userId);
+  }
+
+  @override
+  Future<void> updateQuantity(int cartItemId, int userId, int newQuantity) async {
+    await remoteDataSource.updateQuantity(cartItemId, userId, newQuantity);
+  }
+
+  @override
+  Future<void> deleteItem(int cartItemId, int userId) async {
+    await remoteDataSource.deleteItem(cartItemId, userId);
+  }
+}
