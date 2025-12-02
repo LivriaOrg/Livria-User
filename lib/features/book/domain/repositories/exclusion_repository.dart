6f77650b5a7b1c2cada9aceae8f807d9ef13834d
kistoo <tomio.nakamura15@gmail.com>
2025-12-01 21:50:15 -0500

    Add exclusion feature

---
+++ lib/features/book/domain/repositories/exclusion_repository.dart
@@ -0,0 +1,16 @@
+abstract class ExclusionRepository {
+  Future<void> addToExclusions({
+    required int userId,
+    required int bookId,
+  });
+
+  Future<void> removeFromExclusions({
+    required int userId,
+    required int bookId,
+  });
+
+  Future<bool> checkIsExcluded({
+    required int userId,
+    required int bookId,
+  });
+}
